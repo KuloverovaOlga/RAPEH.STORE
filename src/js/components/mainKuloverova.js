dac2cc590f9d92e8a1c3c39cc79a485210344cb1
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-12-27 15:57:22 +0400

    add line order

--- src/js/components/mainKuloverova.js
+++ src/js/components/mainKuloverova.js
@@ -134,5 +134,23 @@ document.addEventListener('DOMContentLoaded', () => {
     } catch {}
 
 
+// const lineWrapper = document.querySelectorAll('.order-detail__information-status-element')
+
+// lineWrapper.forEach((item, i) => {
+// const line = item.querySelector('.line')
+// const text = item.querySelector('.order-detail__information-status-text')
+// const circle = item.querySelector('.order-detail__information-status-circle')
+
+
+// // Извлечение ширины из объекта
+// var widthItem = (item.offsetWidth  - circle.offsetWidth) / 2 ;
+
+
+// console.log(widthItem)
+// if (line) {
+//     line.style.width = ` ${widthItem}px`
+// }
+// })
+
 });
 
