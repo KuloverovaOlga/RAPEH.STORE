0128ce130d4e9babceba74463548e9a028c7d6eb
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-12-27 22:54:44 +0400

    fix about page

--- src/js/components/articlesTabs.js
+++ src/js/components/articlesTabs.js
@@ -8,17 +8,9 @@ buttonsWrap.addEventListener('click', (e) => {
     if (target.classList.contains('articles__filter-btn') &&  !target.classList.contains('articles__filter-btn--first') ) {
         buttons.forEach(item => item.classList.remove('isActive'))
         target.classList.add('isActive')
-        firstBlock.style.maxHeight = '0'
-        firstBlock.style.marginBottom = '0'
-        
-        setTimeout(()=> {
-            firstBlock.style.transform = 'scale(0)'
-            
-        },0)
-        setTimeout(()=> {
             firstBlock.style.display = 'none'
             
-        },300)
+ 
 
     }
     if (target.classList.contains('articles__filter-btn--first') ) {
@@ -26,22 +18,11 @@ buttonsWrap.addEventListener('click', (e) => {
         target.classList.add('isActive')
         firstBlock.style.display = 'grid'
         
-        setTimeout(()=> {
-            firstBlock.style.transform = 'scale(1)'
-            firstBlock.style.marginBottom = window.innerWidth>768?'6rem': '4.8rem'
-            firstBlock.style.maxHeight = window.innerWidth>768? '146rem' :'222rem'
-        },10)
 
     }
 })
 
-firstBlock.style.marginBottom = window.innerWidth > 768 ?'6rem': '4.8rem'
-firstBlock.style.maxHeight = window.innerWidth > 768 ? '146rem' :'222rem'
 
-window.addEventListener('resize', () => {
-    firstBlock.style.marginBottom = window.innerWidth > 768 ?'6rem': '4.8rem'
-    firstBlock.style.maxHeight = window.innerWidth > 768 ? '146rem' :'222rem'
-})
 
 // const paginationBtns = document.querySelectorAll('.articles__pagination-btn')
 // paginationBtns.forEach(item => {
