2ef8db75db9efa673e5063a205e14bb460a4c3ef
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2024-03-25 14:27:24 +0400

    fix mail input

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,27 +1,31 @@
 const burgerMenu = () => {
 
-        const burger = document.querySelector('.burger');
+        const burgers = document.querySelectorAll('.burger');
         const navWrapper = document.querySelector('.header');
         const nav = document.querySelector('.header__desk-nav');
     
         function openBurger() {
             document.body.style.overflow = 'hidden';
             navWrapper.classList.remove('fixed-nav');
-            burger.classList.add('active');
+            burgers.forEach(burger =>  burger.classList.add('active') )
+          
             nav.classList.add('active');
 
         }
     
         function closeBurger() {
             document.body.style.overflow = '';
-            burger.classList.remove('active');
+            burgers.forEach(burger =>   burger.classList.remove('active') )
+           
             nav.classList.remove('active');
       
         }
-    
-        burger.addEventListener('click', () => {
-            burger.classList.contains('active') ? closeBurger() : openBurger();
-        });
+        burgers.forEach(burger=> {
+            burger.addEventListener('click', () => {
+                burger.classList.contains('active') ? closeBurger() : openBurger();
+            });
+
+        } )
 
         nav.addEventListener('click', (e) => {
     
